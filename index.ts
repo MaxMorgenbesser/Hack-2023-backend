5715fef151494cff7c24ce05c45e2dd6a36f8cc5
Ralph Sébastien Placide <placideralph@gmail.com>
2023-09-30 11:10:25 -0400

    deploy firebase

--- index.ts
+++ index.ts
@@ -1,19 +1,22 @@
-import express, { Express }  from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import * as functions from 'firebase-functions';
+import express, { Express } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 import UserRouter from './src/user/UserRouter';
 
 dotenv.config();
-const app: Express = express()
+const app: Express = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const port = process.env.PORT
+// const port = process.env.PORT
 
-app.use('/user', UserRouter)
+app.use('/user', UserRouter);
 
-  app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  });
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// });
+
+export const api = functions.https.onRequest(app);
